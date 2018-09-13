24cb2961fd8327689a5150955d85eebe15fa740a
Ilya Zlatkin <ilyazlatkin@Ilyas-MacBook-Pro.local>
2018-09-13 22:24:34 +0300

    typo

--- src/main/java/OpenWayTask.java
+++ src/main/java/OpenWayTask.java
@@ -1,14 +1,14 @@
 import java.util.Scanner;
 
 public class OpenWayTask {
-    public static String INCORRECT_NUMBER_FORMATE = "Incorrect Number Format";
+    public static String INCORRECT_NUMBER_FORMAT = "Incorrect Number Format";
 
     public static String f_1_dev_x(String value){
         try {
             double out = 1.0 / (Double.parseDouble(value));
             return String.valueOf(out);
         }catch (java.lang.NumberFormatException e){
-            return INCORRECT_NUMBER_FORMATE;
+            return INCORRECT_NUMBER_FORMAT;
         }
     }
 
@@ -17,9 +17,9 @@ public class OpenWayTask {
         Scanner scanner = new Scanner(System.in);
         String inputString = scanner. nextLine();
         String result =  f_1_dev_x(inputString);
-        if(!result.equals(INCORRECT_NUMBER_FORMATE))
+        if(!result.equals(INCORRECT_NUMBER_FORMAT))
             System.out.println("1 / " + inputString + " = " + f_1_dev_x(inputString));
         else
-            System.out.println(INCORRECT_NUMBER_FORMATE);
+            System.out.println(INCORRECT_NUMBER_FORMAT);
     }
 }
